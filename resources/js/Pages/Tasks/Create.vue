1589edbb3f874326f083dc154fdd783f2a556ff7
sanjithsarkar <sanjithsarkar2@gmail.com>
2023-04-30 14:50:09 +0600

    Initial commit

---
+++ resources/js/Pages/Tasks/Create.vue
@@ -0,0 +1,71 @@
+
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { useForm } from '@inertiajs/vue3'
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+const form = useForm({
+    name: '',
+})
+
+</script>
+<template>
+    <AuthenticatedLayout>
+        <div class="w-full flex justify-center">
+            <div class="sm:w-full lg:w-10/12 shadow-md bg-white rounded border">
+                <div class="w-full flex justify-center">
+                    <h3 class="text-lg font-bold uppercase mt-5 mb-5">Insert Tast</h3>
+                </div>
+
+                <div class="w-full flex justify-center">
+                    <div class="md:w-9/12  shadow-md rounded bg-white border">
+
+                        <h4 class="flex justify-center mt-3 mb-5">Create Brand</h4>
+                        <form class="flex justify-center mb-5" @submit.prevent="insert">
+                            <div>
+                                <div>
+                                    <InputLabel for="name" value="Brand" />
+
+                                    <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required
+                                        autofocus />
+
+                                    <InputError class="mt-2" :message="form.errors.name" />
+                                </div>
+
+                                <!-- <div>
+                                    <InputLabel for="image" value="Brand" />
+
+                                    <input id="image" type="file" @input="form.image = $event.target.files[0]" />
+
+                                    <InputError class="mt-2" :message="form.errors.image" />
+
+
+
+                                </div> -->
+
+                                <!-- <progress v-if="form.progress" :value="form.progress.percentage" max="100">
+                                    {{ form.progress.percentage }}%
+                                </progress> -->
+
+                                <!-- <div class="flex items-center justify-end mt-4">
+
+
+                                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }"
+                                        :disabled="form.processing">
+                                        Save Brand
+                                    </PrimaryButton>
+                                </div> -->
+
+
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/Tasks/Index.vue
@@ -0,0 +1,106 @@
+
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Link } from '@inertiajs/vue3'
+
+</script>
+<template>
+    <AuthenticatedLayout>
+        <div class="w-full flex justify-center">
+            <div class="sm:w-full lg:w-10/12 shadow-md bg-white rounded border">
+                <div class="w-full">
+                    <!-- <Link href="/tasks/create"
+                        class="ml-28 mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-6 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    Add Task
+                    </Link> -->
+                    <Link :href="route('tasks.create')">
+                    <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+                    Add Task
+                    </Link>
+                    <h3 class="inline text-lg font-bold uppercase mt-5 mb-5 ps-96">Task list</h3>
+                </div>
+
+
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Product name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    <div class="flex items-center">
+                                        Color
+                                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1"
+                                                aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
+                                                <path
+                                                    d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                                            </svg></a>
+                                    </div>
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    <div class="flex items-center">
+                                        Category
+                                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1"
+                                                aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
+                                                <path
+                                                    d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                                            </svg></a>
+                                    </div>
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    <div class="flex items-center">
+                                        Price
+                                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1"
+                                                aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
+                                                <path
+                                                    d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                                            </svg></a>
+                                    </div>
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    <div class="flex items-center">
+                                        Action
+                                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1"
+                                                aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
+                                                <path
+                                                    d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                                            </svg></a>
+                                    </div>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td class="px-6 py-4">
+                                    Silver
+                                </td>
+                                <td class="px-6 py-4">
+                                    Laptop
+                                </td>
+                                <td class="px-6 py-4">
+                                    $2999
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="#"
+                                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">View</a>
+
+                                    <a href="#"
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</a>
+
+                                    <a href="#"
+                                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">delete</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
